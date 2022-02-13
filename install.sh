09b9ec325c8809b0a167399bc1cbcefdc8414d50
Mary Marchini <oss@mmarchini.me>
2022-02-12 20:41:55 -0800

    install: enable install and update from script

--- install.sh
+++ install.sh
@@ -3,12 +3,12 @@
 set -xe
 export DOTFILES_PATH=${DOTFILES_PATH:-"$HOME/.dotfiles"}
 
-# if [ ! -d $HOME/.dotfiles ]; then
-#   git clone git@github.com:mmarchini/dotfiles $DOTFILES_PATH
-# else
-#   cd $DOTFILES_PATH
-#   git pull --ff-only
-# fi
+if [ ! -d $HOME/.dotfiles ]; then
+  git clone git@github.com:mmarchini/dotfiles $DOTFILES_PATH
+else
+  cd $DOTFILES_PATH
+  git pull --ff-only
+fi
 
 for install in "${DOTFILES_PATH}"/*/install.sh; do
   "${install}"
