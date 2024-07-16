aff30dc756bc2210d93e683e2e18f88282d8ef94
Mary Marchini <oss@mmarchini.me>
2024-07-15 17:36:32 -0700

    alacritty initial setup

---
+++ alacritty/install.sh
@@ -0,0 +1,14 @@
+#!/bin/zsh
+
+set -xe
+export DOTFILES_PATH=${DOTFILES_PATH:-"$HOME/.dotfiles"}
+
+if ! which alacritty; then
+  if which apt-get > /dev/null; then
+    sudo apt-get install -y alacritty
+  else
+    brew install alacritty
+  fi
+fi
+
+echo 'import = [ "'"${DOTFILES_PATH}/alacritty/alacritty.toml"'", ]' > "$HOME/.alacritty.toml"
